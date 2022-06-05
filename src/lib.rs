439a43475054728be74c15203c3b3727765b3dd4
JinVan <admin@660.moe>
2022-06-05 15:46:55 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -368,7 +368,7 @@ impl Build {
 
             if !target.contains("uwp") {
                 build.define("ZMQ_HAVE_IPC", "1");
-                build.define("ZMQ_HAVE_OPENPGM", "1");
+                build.define("WITH_OPENPGM", "1");
             }
         } else if target.contains("linux") {
             create_platform_hpp_shim(&mut build);
